a2a971f3e5c8d3ff8e14a907108a16bb12b33e9f
claudioborja <claudio-borja@superiorapch.edu.ec>
2022-08-12 17:01:56 -0500

    variables

---
+++ src/Ej2_Variables.java
@@ -0,0 +1,19 @@
+public class Ej2_Variables {
+    public static void main(String[] args) {
+        int speed;
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String Name = "Claudio Borja";
+
+        System.out.println("My name is, "+ Name + " and my salary is " + salary);
+
+        int bonus = 200;
+        salary += bonus;
+
+        System.out.println("My bonus is " + bonus + " and my salary whit bonus is " + salary);
+
+
+    }
+}
