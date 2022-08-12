9ca86ee5f6c6e5d2130420e9da8748642d134e49
claudioborja <claudio-borja@superiorapch.edu.ec>
2022-08-12 18:00:37 -0500

    Cast

---
+++ src/Ej5_OperacionesMatematicas.java
@@ -0,0 +1,13 @@
+public class Ej5_OperacionesMatematicas {
+    public static void main(String[] args) {
+        double numberOne = 2.1;
+        double numberTwo = 3;
+
+        System.out.println(Math.ceil(numberOne));
+        System.out.println(Math.floor(numberTwo));
+        System.out.println(Math.pow(numberOne,numberTwo));
+        System.out.println(Math.max(numberOne,numberTwo));
+        System.out.println(Math.sqrt(numberTwo));
+        System.out.println((4/3)*Math.PI*Math.pow(numberTwo,3));
+    }
+}
